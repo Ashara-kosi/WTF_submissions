c415f96781348c00c0bc59517bf36a689c0df862
Mar-lim <mariamalimi5@gmail.com>
2023-12-12 23:23:21 +0100

    Mariam Alimi, this is my commit message

---
+++ calculator.py
@@ -0,0 +1,15 @@
+x=int(input("First Value: "))
+y=int(input("Second Value: "))
+
+
+print(f"Add \n{x} + {y} =")
+print(x+y)
+
+print(f"Subtract \n{x} - {y} =")
+print(x-y)
+
+print(f"Multiply \n{x} * {y} =")
+print(x*y)
+
+print(f"Divide \n{x} / {y} =")
+print(x/y)
