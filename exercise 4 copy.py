eb23cab2839166e101c9abac22a4018f4ab933be
ChideraFrancisca <mbafrancisca611@gmail.com>
2023-12-13 00:48:39 +0100

    ChideraFrancisca

---
+++ exercise 4 copy.py
@@ -0,0 +1,114 @@
+import math
+#number  
+
+# print("+",end=" ")
+# for i in range(4):
+#     print("-",end=" ")
+# print("+")
+
+# list1=["+----+", "\    /", "/    \\","\    /","/   \\","\    /","/    \\","+----+"]
+# for i in list1:
+#     print(i)
+
+#number2
+#*******
+#*******
+#*******
+#*******
+#*******
+ 
+# for i in range(5):
+#     for j in range(7):
+#         print("*", end=" ")
+#     print()
+
+#number 3
+
+# a.for i in range (1,7):
+#     print(i)
+
+# b. for i in range (2,13,2):
+#     print(i)
+
+# c.for i in range (4,80,15):
+#     print(i)
+
+#d for i in range (30,-21,-10):
+#     print (i)
+#e
+# for i in range(-7,14,4):
+#     print(i)
+
+#number 4
+# for i in range(7):
+#      for j in range(1,i+1):
+#         print(j,end=" ") 
+#      print()
+        
+#number 5
+# def pay(salary, no_hours):
+#     if no_hours>8:
+#         overtime=1.5 * salary
+#         money= salary * no_hours + overtime
+#     else:
+#         money = salary +  no_hours
+#     return money
+
+# TA_SALARY=float(input("enter salary"))
+# TA_HOURS=int(input("enter no_hours"))
+
+# payment=pay(TA_SALARY, TA_HOURS)
+# print(payment)
+
+# def area_of_circle(radius):
+#     area = math.pi * radius**2
+#     return area
+
+# radius =float(input("enter radius: "))
+# circle_area =area_of_circle(radius)
+# print(circle_area)
+
+# low = 1
+# high = 1001
+# sum = 0
+# for i in range (low, high):
+#     sum += 1
+#     print ("sum = ", sum)
+
+# low_number = int(input("enter low number: "))
+# high_number = int(input("enter high number: "))
+# sum = 0
+# for i in range (low_number, high_number):
+#     sum += 1
+#     print ("sum = ", sum)
+
+# num = int(input("enter a number: "))
+# second_number = (1)
+# while num != 0:
+#     num = int(input("enter a number: "))
+#     print(num - second_number)
+
+# num =int(input("enter number :" ))
+# greet =(input("enter greeting: "))
+# def rep1 (greet,num):
+#     if num == 0 :
+#         return ("")
+#     else:
+#         return greet * num
+# result = rep1 (greet, num)
+#print (result)
+
+
+
+    
+
+
+#     print(num)
+
+ 
+
+
+
+
+
+
