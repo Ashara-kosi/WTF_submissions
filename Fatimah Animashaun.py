7284169b1605c780f86ed3f23b192ea6a41aafcf
fatimahanimashaun <fatimahanimashaun@womentechsters.org>
2023-12-13 12:29:10 +0100

    fatimah Git pull

---
+++ Fatimah Animashaun.py
@@ -0,0 +1,18 @@
+# num1 = float(input('Put first number'))
+# op = input('What is the operator symbol:')
+# num2 = float(input('Put second number'))
+# op = input('What is the operator symbol:')
+# num3 = float(input('Put third number'))
+# if (op=='+'):
+#     print(num1+num2)
+# elif(op=='-'):
+#     print(num1-num2)
+# elif(op=='*'):
+#     print(num1*num2)
+# elif(op=='/'):
+#     print(num1/num2)
+# elif(op=='%'):
+#     print(num1%num2)
+# else:
+#     print('Invalid operator')
+print(6 + (18 % (17 - 12)))
