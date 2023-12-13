c7f00db04fd8c98ec7a6e77285706ecc36212954
Goodysplendor <aloysiusgoodness@gmail.com>
2023-12-13 10:29:30 +0100

    Goodness Umahi-Godswill

---
+++ Goodness.Umahi-Godswill.py
@@ -0,0 +1,15 @@
+# A Calcultor program that can add, subtract, multiply and divide based on the user's input by Goodness Umahi-Godswill
+# Email: Goodness.Umahi-Godswill@womentechsters.org
+
+numb1 = int(input("Enter first number: "))
+numb2 = int(input("Enter second number: "))
+
+add = (numb1 + numb2)
+subtract = (numb1 - numb2)
+multiply = (numb1 * numb2)
+divide = (numb1 / numb2)
+
+print("Addition is: ",add)
+print("Subtraction is: ",subtract)
+print("Multiplication is: ",multiply)
+print("Diviion is: ",divide)
