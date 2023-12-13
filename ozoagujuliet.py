cc6be4adbbd33d5be3b1ec08c909f7f33dc526f3
Your Nameozoagumary clear <you@example.comozoagumary02@gmail.com>
2023-12-13 13:33:28 +0100

    Ozoagu Juliet

---
+++ ozoagujuliet.py
@@ -0,0 +1,29 @@
+#display a menu for the user to choose from
+# print("Select an operation to perform")
+# print("1.ADD")
+# print("2.SUBTRACT")
+# print("3.MULTIPLY")
+# print("4.DIVIDE")
+#To capture what the user has inputted
+# operation = input()
+
+# if operation == "1":
+#     Number_1 = input("Enter first number")
+#     Number_2 = input("Enter second number")
+    # Sum = str(int(Number_1 )) + str(int(Number_2))
+    # print(Sum)
+#     print("Sum =" + str(int(Number_1) + int(Number_2 )))
+# elif operation == "2":
+#     Number_1 = input("Enter first number")
+#     Number_2 = input("Enter second number")
+#     print("Diference =" + str(int(Number_1) - int(Number_2 )))
+# elif operation == "3":
+#     Number_1 = input("Enter first number")
+#     Number_2 = input("Enter second number")
+#     print("product =" + str(int(Number_1) * int(Number_2 )))
+# elif operation == "4":
+#     Number_1 = input("Enter first number")
+#     Number_2 = input("Enter second number")
+#     print("Result =" + str(int(Number_1) / int(Number_2 )))
+# else:
+#         print('invalid Entery')
