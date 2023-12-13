3b155d2808d389c50ad529688f6292b96c59e7c5
Your Nameozoagumary clear <you@example.comozoagumary02@gmail.com>
2023-12-13 14:06:21 +0100

    Ozoagu Juliet Olucchi

---
+++ class1.py
@@ -0,0 +1,69 @@
+print("Hello World")
+#congratulations to me, i just printed my first line of code in python
+print("My name is JULIET")
+#syntax in python is like following the rules and regulations in python, failure to follow these set of rules will throw an error in your code
+#indentation
+#indentation error means your code aint aligned properly as supposed to eg
+#  print("My surname is ozoagu")
+
+#IDE-INTEGRATED DEVELOPMENT ENVIRONMENT: THESE ARE VARIOUS PLATFORMS WHERE WRITING OF CODES, DEBUGGING AND TRACING VARIOUS VARIABLES IS BEING CARRIED OUT
+# Examples of an IDE includr pycham. vs code, jupyter notebook, glogle collab
+#EPRESSION IN PYTHON
+# 1. PRINT FUNCTION : It is a way of commanding the python to reture an output when an input is being made or given . example
+print( 1+4+3)
+print(42)
+#note that STRING means a character or text which must be put in quote while
+# integers are numbers which are not put in quote
+print(1+3*4)
+# a variable is like when you asign something. it is like a placeholder. examples
+a = 1+3*4
+print(a)
+# this implies that we want to put 1+3*4 in a. ie whenever we call a the sum and multiplication of this should be display
+b = (1+3)*4
+print(b)
+print(25)
+#DIVISION in Python is represented using  two backward slash// examples.
+a= 35//5
+print(a)
+b= 145//10
+print(b)
+# note that when we divided 145 by 10 , it output 14 instead of 14.5. This to tell us that in using a divison , only the integer or whole number is shown but to view the remainder, MODULES IS BE USED.
+b = 145 % 10
+print(b)
+#MODULES is represented by %
+#print() parameters in python. Parameters are what enters a function when it is being called
+#print("my name is Ozoagu" , sep="," , end="Juliet Oluchi")
+#print("my name is Ozoagu" , "," , end="Juliet Oluchi")
+
+#Escape Sequences examples
+# print("my name is juliet \t I am a winner")
+#print("my name is juliet \n I am a winner")
+#print("my name is juliet \b I am a winner")
+#print("Juliet saidd 'she is a winner'")
+
+# Getting an information from a user. if may maybe you are coding something and it requires you to get an information from a user, INPUT FUNCTION IS BE USED TO DO THAT
+#name =input("what is your name")
+#print(name)
+#print('{2} {1} {0}'.format('Directions','Read','The'))
+
+#Num1 = int(input("what is your first number"))
+#Num2 = int(input("what is your second number"))
+#Ans = Num1 + Num2
+#print(f'{Num} + {Num2} = {Ans}')
+#name = input("what is your name")
+#print(name)
+# num1 = int(input("what is your first number"))
+# print(num1)
+#num2 = int(input("what is your second number"))
+#ans = num1 + num2
+#print(f'{num1} + {num2} = {ans}')
+#rint("Invalid input. Please enter valid numbers.")
+
+
+
+
+
+
+
+
+
