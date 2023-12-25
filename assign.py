dc22a2e2dc91eeb0a626eef5b0747c21b6e0f82a
kvexi <kweenroyalty5@gmail.com>
2023-12-25 21:01:06 +0100

    king vessy

---
+++ assign.py
@@ -0,0 +1,49 @@
+#print("king vexu")
+#print("my name is vesi 'i have a benz'")
+#name=input("what is your name")
+#print(name)
+#print('{2} {1} {0}'.format('directions','the','Read'))
+#print(f'{name} went to the market.')
+#a=int(input('please put in the numb'))
+#b=int(input('please put the 2nd numb'))
+#c=a+b
+#print(f'{a}+{b}={c}')
+#z=718.89
+#q= float(z)
+#print(q)
+list_a=[1,3,5,6,8,11,23,43]
+list_dav=[23,89,73,21.67,8.2,'king','author']
+#print(list_dav[5],list_dav[6],list_a[5])
+#list_dav[5]='Queen'
+#print(list_dav)
+#list_a.append('Camlot')
+#list_dav.pop(6)
+#print(list_dav)
+#list_a.sort(reverse=False)
+#print(list_a)
+#listz=list_a.copy
+#listz.pop(4)
+#print(list_a[0:])
+#print(list_dav[5][1])
+#kv_list=[_for_ in 'abcdefgh']
+#print(list_dav[::-2])
+#print(list_dav[6:3:-1])
+#print('Hello, \n USA')
+#s='"python\'s is awesome" she said'
+#numb=['man','girl','woman']
+#listn='\n'.join(numb)
+#s=f'The numbers listed are:\n {listn}
+#print('c:\nin\the downward spiral')
+#print(' / \\ // \\\\ /// \\\\\\') = to get \\ double it to get that as output
+#print('"This quote is from \n Irish poet Oscar Wilde: \n "Music makes one feel so romantic \n - at least it always gets on ones nerve"s -')
+#print('"which is the same thing nowadays."')
+#S='happine'
+#print(S[len(S)//2+1:])
+#print(6+(18%(17-12)))
+#print(248%100/5)
+#tuple1:tuple[int|str,...]=(1,"king","venom",6,7)
+fem=('girl','code','super','black',10,)
+(*gen,race,age)=fem
+print(gen)
+print(race)
+print(age)
