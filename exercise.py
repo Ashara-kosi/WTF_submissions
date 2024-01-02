7b00d0e8b1c3ae80c6677968aadcfc8dad329693
lynattenantunga <lynete98@gmail.com>
2024-01-02 16:36:03 +0300

    Removed naming conflict

---
+++ exercise.py
@@ -0,0 +1,25 @@
+#number 1
+#print("\ta\tb\tc")
+#print("\\\\")
+#print("'")
+#print("\"\"\"")
+#print("c:\nin\the downward spiral")
+
+#number 2
+#print("/\//\\\///\\\\\\")
+
+#number 3
+#print('This quote is from\nIrish poet Oscar Wilde:\n "Music makes one feel so romantic\n - at least it always gets on one\'s nerves -\n which is the same thing nowadays."')
+
+#number 4
+#print('A "quoted" string is\n \'much\' better if you learn\n the rules of "escape sequences."\n Also,"" represents an empty string.\n Don\'t forget: use \\" instead of \" !\n \'\' is not the same as \"' )
+
+#number 5
+print(-9/5)
+print(695%20)
+print(7+6*5)
+print(7*6+5)
+print(248%100/5)
+print(6*3-9/4)
+print((5-7)*4)
+print(6+(18%(17-12)))
